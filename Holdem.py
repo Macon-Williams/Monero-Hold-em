839d5bfa877571599e73970595d399699343a363
Macon Williams <williamscmacon@gmail.com>
2020-10-28 13:36:51 -0600

    Added roles for each player
    Each player now gets assigned a role when the game begins (dealer, small blind, big blind)
    
    As each round is over, the infastructure is set up to handle the rotation of roles. (The player to the left of the dealer is now dealer, etc.)

--- Holdem.py
+++ Holdem.py
@@ -7,7 +7,7 @@ from Table import Table
 
 def player_draw(deck, player):
     for i in range(2):
-        for j in range(len(player)):
+        for j in player:
             player[j].cards.append(deck.give_first_card())
 
 
@@ -40,16 +40,31 @@ def initial_bet(deck, table, player):
 class Holdem(object):
     deck = deck_of_cards.DeckOfCards()
     player = []
+    role = ["deal", "bb", "sb"]
     num_of_players = int(input("How many players? (1-9)"))
     for i in range(num_of_players):
         name_of_player = input(f"Name {i+1}?")
         player.append(Player(name_of_player, 1, i))
     table = Table()
     deck.shuffle_deck()
-    # Assign initial role (Dealer, little blind, big blind)
+
+    # Assign roles (Dealer, little blind, big blind)
+    # Probably figure out how to make this better, 3 for loops is chonky.
+
+    # Initialize the array
     for i in range(len(player)):
-        if i == 0:
-            player[i].role.append("Dealer")
+        for j in range(len(player)):
+            player[i].role.append("player")
+
+    # Assign dealer, big blind, and small blind rounds
+    for i in range(len(player)):
+        for j in range(len(player)):
+            if j < 3:
+                player[i].role[j] = role[j]
+
+    # Rotate turns around
+    for i in range(len(player)):
+        player[i].role = (player[i].role[len(player[i].role) - i:len(player[i].role)] + player[i].role[0:len(player[i].role) - i])
 
     flop(deck, table)
     table.list_cards()
